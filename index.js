e6201caa08f5a1e50eb85c7d3952d4a89bf165d7
huangkuncan <huangkuncan@meituan.com>
2022-11-28 20:13:29 +0800

    get请求走通

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
-const http = require('http');
-const server = http.createServer();
-server.on('request', function (req, res) {
-  console.log('收到网络请求');
-  res.end('huangkucnan');
-});
-server.listen(8080, function () {
+var express = require('express');
+var app = express();
+
+
+
+app.get('/', (req, res) => {
+  res.send('huangkucan Hello World!')
+})
+
+
+app.get('/login', (req, res) => {
+  console.log('hkc', req.query);
+  const name = req.query?.name;
+  res.send(name + ' login success');
+})
+
+app.get('/redirect', (req, res) => {
+  console.log('hkc', req);
+  res.redirect('http://www.baidu.com');
+})
+
+app.listen(8080, function () {
   console.log('服务器启动成功');
 });
